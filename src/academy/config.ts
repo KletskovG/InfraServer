2ff1143179d35a0b6c0ef9d5b38545994bd2f440
Gleb Kletskov <gfgfddgleb@gmail.com>
2022-04-12 01:33:14 +0300

    update scrape config

--- src/academy/config.ts
+++ src/academy/config.ts
@@ -8,7 +8,6 @@ export const SUBMIT_BUTTON_SELECTOR = "input.button";
 export const PROJECTS_PRESENTED_SELECTOR = ".up-info--check .button--green";
 
 export const scrapeConfig: AcademyScrapeConfig = {
-  REACT: "https://up.htmlacademy.ru/react/9/check/projects",
   JS1: "https://up.htmlacademy.ru/javascript/25/check/projects",
 };
   
