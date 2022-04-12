3fb046bb6c0d953e11421d0e25eaa81534c06666
Gleb Kletskov <gfgfddgleb@gmail.com>
2022-04-12 23:14:27 +0300

    Add new command to bot

--- src/telegram/bot.ts
+++ src/telegram/bot.ts
@@ -1,5 +1,6 @@
 import { Telegraf } from "telegraf";
 import { getEnvVariable } from "utils/getEnvVariable";
+import { scrapeProjectInfo } from "academy";
 
 const BOT_TOKEN = getEnvVariable("BOT_TOKEN");
 const CHAT_NUMBER = getEnvVariable("CHAT_NUMBER");
@@ -9,6 +10,25 @@ bot.launch();
 
 bot.on("message", (ctx) => {
   ctx.reply(`Your Chat ID ${ctx.chat.id}`);
+});
+
+bot.command("academy", async (ctx) => {
+  try {
+    scrapeProjectInfo()
+      .then(result => {
+        if (result) {
+          const notification = `Scrape result \n ${result}`;
+          ctx.reply(notification);
+        } else {
+          ctx.reply("SMTH WENT WRONG");
+        }
+      })
+      .catch(() => {
+        ctx.reply("ERROR WHILE SCRAPE");
+      });
+  } catch (error) {
+    sendNotification(error);
+  }
 });
 
 export function sendNotification(message: string) {
