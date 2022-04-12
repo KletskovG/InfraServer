0aa700f91308620755f369c34797010ffe330666
Gleb Kletskov <gfgfddgleb@gmail.com>
2022-04-12 23:10:12 +0300

    Add chat id info

--- src/telegram/bot.ts
+++ src/telegram/bot.ts
@@ -7,6 +7,10 @@ const CHAT_NUMBER = getEnvVariable("CHAT_NUMBER");
 const bot = new Telegraf(BOT_TOKEN);
 bot.launch();
 
+bot.on("message", (ctx) => {
+  ctx.reply(`Your Chat ID ${ctx.chat.id}`);
+});
+
 export function sendNotification(message: string) {
   bot.telegram.sendMessage(CHAT_NUMBER, message);
 }
