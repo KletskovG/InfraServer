93bed8aca684eb62ea371a996ba241cde5effdd9
Gleb Kletskov <gfgfddgleb@gmail.com>
2022-05-08 13:41:30 +0300

    update airtag tracking

--- src/telegram/airtagLocation.ts
+++ src/telegram/airtagLocation.ts
@@ -1,21 +1,20 @@
 import { sendNotification }  from "telegram/bot";
 
+
 let isActive = false;
 const checkTagInterval = 1000 * 60 * 10; // 10 minutes
 
-export function toggleTagMode() {
-  let interval;
-  const intervalCallback = () => {
+const intervalCallback = () => {
+  if (isActive) {
     sendNotification("Check airtaglocation \n https://www.icloud.com/shortcuts/916de19e281744d7b60012b453805354");
-  };
+  }
+};
 
+setInterval(intervalCallback, checkTagInterval);
+export function toggleTagMode() {
   if (isActive) {
     isActive = false;
-    clearInterval(interval);
-    console.info("SWITCHING OFF AIRTAG TRACKING");
   } else {
     isActive = true;
-    interval = setInterval(intervalCallback, checkTagInterval);
-    console.info("SWITCHING ON AIRTAG TRACKING");
   }
 }
