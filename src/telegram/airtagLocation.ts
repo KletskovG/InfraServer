9b3b66fd677019e98a7d9cb1e92a79b762fdaaf3
Gleb Kletskov <gfgfddgleb@gmail.com>
2022-05-10 22:03:42 +0300

    add redirect

--- src/telegram/airtagLocation.ts
+++ src/telegram/airtagLocation.ts
@@ -6,7 +6,7 @@ const checkTagInterval = 1000 * 60 * 10; // 10 minutes
 
 const intervalCallback = () => {
   if (isActive) {
-    sendNotification("Check airtaglocation \n https://www.icloud.com/shortcuts/916de19e281744d7b60012b453805354");
+    sendNotification("Check airtaglocation \n https://telegram.kletskovg.tech/cash");
   }
 };
 
