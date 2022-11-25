5b03d334f4a6114c8be7ace491941062e7f50a25
Kletskov Gleb <gfgfddgleb@gmail.com>
2022-11-25 23:44:22 +0300

    update deploy process

--- src/index.ts
+++ src/index.ts
@@ -20,5 +20,5 @@ app.listen(PORT, () => {
     `Infra server is up and running // UPDATED
       http://localhost:${PORT}`
   );
-  sendNotification("Infra server is booted on a new VPS");
+  sendNotification("Infra server is booted with new deploy");
 });
