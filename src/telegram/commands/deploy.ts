08a4704cb37e5e6e685095319f54fb2671a8eff3
Kletskov Gleb <gfgfddgleb@gmail.com>
2022-12-05 13:10:59 +0300

    check update

--- src/telegram/commands/deploy.ts
+++ src/telegram/commands/deploy.ts
@@ -2,6 +2,6 @@ import got from "got/dist/source";
 import TelegrafContext from "telegraf/typings/context";
 
 export function deploy(ctx: TelegrafContext) {
-  got("http://139.59.151.135:8091/update")
-    .then((res) => ctx.reply(res.body));
+  got("http://139.59.151.135:8091/update");
+  ctx.reply("Wait for new message");
 }
