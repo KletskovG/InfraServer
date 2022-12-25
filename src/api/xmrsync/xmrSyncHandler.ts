d3292d0e36df9b1d44dfdeee3864aa05ef902fa5
Kletskov Gleb <gfgfddgleb@gmail.com>
2022-12-25 23:11:48 +0300

    Update

--- src/api/xmrsync/xmrSyncHandler.ts
+++ src/api/xmrsync/xmrSyncHandler.ts
@@ -10,8 +10,9 @@ export function xmrSyncHandler(req: IRequest<DoneMessageParams>, res: Response) 
   const {
     name = "Name not set",
     status = "Status unknown",
+    container = "Container uknown"
   } = req.query;
-  const message = `XMR SYNC \n Node ${name} \n Status: ${decodeURI(status)}`;
+  const message = `XMR SYNC \n Node ${name} \n Container ${container}  \n Status: ${decodeURI(status)} `;
   sendNotification(message);
   res.status(200).send("OK");
 }
